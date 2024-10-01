b91b928b0e9b9ee2c50322aedff4bac8d7d6f9e0
polarune(laptop) <jumpcross123@naver.com>
2024-10-01 13:29:35 +0900

    bg 추가, 소년의 처음 위치 설정

--- boy_move_handarrow.py
+++ boy_move_handarrow.py
@@ -7,4 +7,30 @@ open_canvas()
 bg = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+
+running = True
+x = 800 // 2
+y = 600 // 2
+frame = 0
+
+while running:
+    clear_canvas()
+    bg.draw(400, 300)
+    character.clip_draw(frame*100,100,100,100,x,y)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+
 close_canvas()
